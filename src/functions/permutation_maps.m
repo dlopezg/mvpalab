ab88d0a70014074666aa694489b6363a28ec757f
David López-García <dlopez@ugr.es>
2019-08-10 00:18:22 +0200

    svm_classifier > Accuracy calculation has been accelerated.

--- src/functions/permutation_maps.m
+++ src/functions/permutation_maps.m
@@ -1,7 +1,6 @@
 function [ permuted_maps ] = permutation_maps( cfg, inputvec )
 %PERMUTATION_MAPS This function generates permutation maps at a subject
 %level for future statistical analyses.
-
 fprintf('      <strong>> Generating permutation maps...</strong>\n');
 
 %% Data and true labels:
@@ -15,16 +14,18 @@ X = [class_a;class_b];
 Y = logical([zeros(size_a,1);ones(size_b,1)]);
 
 %% Generate permuted labels
-per_Y = repmat(Y,1,cfg.permaps.nper);
+per_Y = repmat(Y,1,cfg.stats.nper);
 
-for i = 1 : cfg.permaps.nper
+for i = 1 : cfg.stats.nper
     per_Y(:,i) = Y(randperm(length(Y)));
 end
 
 %% Train and test de classifier with permuted labels:
-for i = 1 : cfg.permaps.nper
+for i = 1 : cfg.stats.nper
+    tic
     permuted_maps(:,:,i) = svm_classifier(X,per_Y(:,i),cfg.mvpa);
-    fprintf([' - Permutation: ' int2str(i) '\n']);
+    fprintf([' - Permutation: ' int2str(i) ' > ']);
+    toc
 end
 
 end
--- src/functions/svm_classifier.m
+++ src/functions/svm_classifier.m
@@ -1,53 +1,53 @@
 function [ correct_rate, cfg] = svm_classifier( X, Y, cfg )
 %SVM_CLASSIFIER This function returns the accuracy of the classifier in a
 %time-resolved way.
-
 strpar = cvpartition(Y,'KFold',cfg.nfolds);
 
 %% Timepoints loop:
 fprintf('       - Timepoints: ');
 n = numel(num2str(cfg.ntp)) + 1;
 
-%% Classperf initialization:
-if isfield(cfg,'tempgen')
-    tempgen = cfg.tempgen;
-    if cfg.tempgen
-        for i = 1 : cfg.ntp
-            for j = 1 : cfg.ntp
-                cpmtx{i,j} = classperf(Y);
-            end
-        end
-    end
-end
-
-
 %% Timepoints loop
+
 for tp = 1 : cfg.ntp
     cp{tp} = classperf(Y);
+    
     %% Cross-validation loop:
     for k = 1 : strpar.NumTestSets
         %% Train and test datasets:
+        test_fold = strpar.test(k);
+        training_fold = strpar.training(k);
         test_X = X(strpar.test(k),:,cfg.tpoints(tp));
-        train_X = X(strpar.training(k),:,cfg.tpoints(tp));
-        train_Y = Y(strpar.training(k));
+        train_X = X(training_fold,:,cfg.tpoints(tp));
+        train_Y = Y(training_fold);
+        test_Y = Y(test_fold);
+        l = length(test_Y);
         
         %% Train SVM model
-        mdlSVM = fitcsvm(train_X,train_Y);
+        mdlSVM = compact(fitcsvm(train_X,train_Y));
         
         %% Test - Temporal generalization matrix:
-        if tempgen
+        if cfg.tempgen
             for tp2 = 1 : cfg.ntp
-                test_X = X(strpar.test(k),:,cfg.tpoints(tp2));
+                test_X = X(test_fold,:,cfg.tpoints(tp2));
                 predictedlabels = predict(mdlSVM,test_X);
-                cpmtx{tp,tp2} = classperf(cpmtx{tp,tp2},predictedlabels,...
-                    strpar.test(k));
-                correct_rate(tp,tp2) = cpmtx{tp,tp2}.CorrectRate;
+                acc(k,tp2) = sum(test_Y == predictedlabels);
+%                 cpmtx{tp,tp2} = classperf(cpmtx{tp,tp2},predictedlabels,test_fold);
+%                 correct_rate(tp,tp2) = cpmtx{tp,tp2}.CorrectRate;
             end
+            
         else
             predictedlabels = predict(mdlSVM,test_X);
-            classperf(cp{tp},predictedlabels,strpar.test(k));
-            correct_rate(tp) = cp{tp}.CorrectRate;
+            acc(k) = sum(test_Y == predictedlabels);
+%             classperf(cp{tp},predictedlabels,strpar.test(k));
+%             correct_rate(tp) = cp{tp}.CorrectRate;
         end
+    end
+    
+    if cfg.tempgen
+        correct_rate(tp,:) = mean(acc/l);
+    else
+        correct_rate(1,tp) = mean(acc/l);
     end
     
     %% Print state:
@@ -59,6 +59,5 @@ for tp = 1 : cfg.ntp
     
     fprintf([int2str(cfg.ntp) '/' int2str(tp)]);
 end
-
 end
 

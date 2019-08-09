70cb618d477bfce850f158eff0a0d9dad9b0e24c
David López-García <dlopez@ugr.es>
2019-08-09 16:53:24 +0200

    mvpa_timing > Changes in cfg structure. The time vector is now saved inside the cfg structure and the function is ready for different types of analyses.

--- src/functions/mvpa_timming.m
+++ src/functions/mvpa_timming.m
@@ -2,22 +2,17 @@ function [ cfg ] = mvpa_timming( cfg )
 %MVPA_TIMMING This function configures the data in the cfg structure for 
 %the mvpa analysis.
 
-c = cfg.mvpa;
 
 %% Find closest points in the time vector:
 
-[~,c.startidx] = min(abs(cfg.times-c.tpstart));
-[~,c.endidx] = min(abs(cfg.times-c.tpend));
+[~,cfg.startidx] = min(abs(cfg.times-cfg.tpstart));
+[~,cfg.endidx] = min(abs(cfg.times-cfg.tpend));
 
 %% Adjust sizes and timepoints:
 
-c.tpoints = (c.startidx:c.tpsteps:c.endidx);
-c.ntp = length(c.tpoints);
-cfg.times = cfg.times(c.startidx:c.tpsteps:c.endidx);
-
-%% Update cfg structure:
-
-cfg.mvpa = c;
+cfg.tpoints = (cfg.startidx:cfg.tpsteps:cfg.endidx);
+cfg.ntp = length(cfg.tpoints);
+cfg.times = cfg.times(cfg.startidx:cfg.tpsteps:cfg.endidx);
 
 end
 

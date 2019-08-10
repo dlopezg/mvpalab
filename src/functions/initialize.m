acc7c2175956eae416c17024fa57818c0c7f6951
David López-García <dlopez@ugr.es>
2019-08-10 18:03:54 +0200

    initialize > A parallel pool is initiated if the toolbox is available.

--- src/functions/initialize.m
+++ src/functions/initialize.m
@@ -1,6 +1,8 @@
 function [ cfg ] = initialize(cfg)
 %INITIALIZE Summary of this function goes here
 %   Detailed explanation goes here
+clc;
+fprintf('<strong> > Initializing MVPA toolbox: </strong>\n');
 
 %% Configuration - Feature extraction:
 cfg.fe.strial.flag     = 1;
@@ -16,8 +18,8 @@ cfg.mvpa.tpsteps = 3;
 % .......................... Kfold:
 cfg.mvpa.nfolds  = 5;
 % .......................... Analysis:
-cfg.mvpa.tempgen = true;
-cfg.mvpa.parcomp = true;
+cfg.mvpa.tempgen = false;
+cfg.mvpa.parcomp = false;
 
 %% Statistic for Stelzer method:
 cfg.stats.nper   = 100;
@@ -28,5 +30,20 @@ cfg.stats.pclust = 99.9;
 %% Plots:
 cfg.plots.stats = false;
 
+%% Other configuration - Parallel computing:
+fprintf('<strong> > Checking parallel computing: </strong>');
+if license('test','Distrib_Computing_Toolbox')
+    fprintf('- Toolbox installed.\n');
+    cfg.mvpa.parcomp = true;
+    p = gcp('nocreate'); 
+    if isempty(p)
+        parpool;
+    end
+else
+    disp('- Toolbox not available');
+    cfg.mvpa.parcomp = false;
+end
+clc
+fprintf('<strong> > MVPATOOLBOX is ready! </strong>\n');
 end
 

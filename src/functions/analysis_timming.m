e11b11d9cfbd02f41bfab678b6095ffd8881ef60
David López-García <dlopez@ugr.es>
2019-08-23 17:07:37 +0200

    analysis_timming > This function configures the time vectors not only for the mvpa analysis but for other analyses.

--- src/functions/analysis_timming.m
+++ src/functions/analysis_timming.m
@@ -1,21 +1,19 @@
-function [ cfg ] = mvpa_timming( cfg )
+function [ cfg ] = analysis_timming( cfg )
 %MVPA_TIMMING This function configures the data in the cfg structure for 
 %the mvpa analysis.
 fprintf('<strong> > Adjusting MVPA timming: </strong>');
-c = cfg.mvpa;
 %% Find closest points in the time vector:
-
-[~,c.startidx] = min(abs(cfg.times-c.tpstart));
-[~,c.endidx] = min(abs(cfg.times-c.tpend));
+[~,cfg.startidx] = min(abs(cfg.times-cfg.tpstart));
+[~,cfg.endidx] = min(abs(cfg.times-cfg.tpend));
 
 %% Adjust sizes and timepoints:
 
-c.tpoints = (c.startidx:c.tpsteps:c.endidx);
-c.ntp = length(c.tpoints);
-c.times = cfg.times(c.startidx:c.tpsteps:c.endidx);
+cfg.tpoints = (cfg.startidx:cfg.tpsteps:cfg.endidx);
+cfg.ntp = length(cfg.tpoints);
+cfg.times = cfg.times(cfg.startidx:cfg.tpsteps:cfg.endidx);
 
 %% Update the cfg structure
-cfg.mvpa = c;
+
 fprintf(' - Done!\n');
 
 end

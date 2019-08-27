6ee595d7c0c9622e422ffa30ff96764fce931652
David López-García <dlopez@ugr.es>
2019-08-27 13:17:46 +0200

    generate_diffmap > This function computes the required diffmaps for the sliding filter analysis.

---
+++ src/functions/generate_diffmap.m
@@ -0,0 +1,24 @@
+function [diffMap,perdiffMap,cfg] = generate_diffmap(cfg,acc,accmap,peracc,permaps)
+
+n_subjects = size(acc,3);
+n_maps = size(permaps,3);
+n_freq = size(accmap,1);
+
+
+%% Generate diffMap for real data and permuted diffMaps for statistics:
+for freq = 1 : n_freq
+    diffMap(freq,:,:) = acc - accmap(freq,:,:);
+    
+    % Generate permuted diffMaps for statistics:
+    if exist('permaps','var') && exist('peracc','var')
+        for map = 1 : n_maps
+            perdiffMap(freq,:,map,:) = peracc - squeeze(permaps(freq,:,map,:));
+        end
+    end
+end
+
+
+
+
+end
+

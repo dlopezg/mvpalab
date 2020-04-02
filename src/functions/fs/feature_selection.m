a9238f4726d64b8f876f48ba417d7481f0605561
David López-García <dlopez@ugr.es>
2020-04-02 11:21:03 +0200

    Bug fix.

--- src/functions/fs/feature_selection.m
+++ src/functions/fs/feature_selection.m
@@ -1,8 +1,8 @@
 function [train_X,test_X,params,cfg] = feature_selection(train_X,train_Y,test_X,test_Y,cfg)
 
-if cfg.pca.flag
+if cfg.analysis.pca.flag
     [coeff,scores,~,~,var,mu] = pca(train_X);
-    train_X = scores(:,1:cfg.pca.ncom,:);
+    train_X = scores(:,1:cfg.analysis.pca.ncom,:);
     %% Project the test set in the principal component space:
     % When you specify a variable weight, the coefficient matrix is not 
     % orthonormal, but the reconstruction rule is still
@@ -10,12 +10,13 @@ if cfg.pca.flag
     % Xcentered/coeff':
     test_X = test_X - mu;            % Xcentered
     test_scores = test_X/coeff';     % Projected scores
-    test_X = test_scores(:,1:cfg.pca.ncom,:);
+    test_X = test_scores(:,1:cfg.analysis.pca.ncom,:);
     
     % Return PCA params:
     params.coeff = coeff; params.mu = mu; params.var = var;
-elseif cfg.pls.flag
-    [xl,yl,xs,ys,beta,pctvar,mse,stats] = plsregress(train_X,train_Y,cfg.pls.ncom);
+elseif cfg.analysis.pls.flag
+    [xl,yl,xs,ys,beta,pctvar,mse,stats] = plsregress(train_X,train_Y,...
+        cfg.analysis.pls.ncom);
 end
 
 end

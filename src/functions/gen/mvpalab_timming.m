d08c47aff5a34c94cdfca51c8c9cef2a1a56c4b6
David López-García <dlopez@ugr.es>
2020-10-29 11:34:54 +0100

    timming > If tpstart and tpend are zero, the entire epoch is analyzed.

--- src/functions/gen/mvpalab_timming.m
+++ src/functions/gen/mvpalab_timming.m
@@ -1,8 +1,15 @@
 function [ cfg ] = mvpalab_timming( cfg )
 %MVPALAB_TIMMING This function configures the data in the cfg structure for 
 %the mvpa analysis.
-% fprintf('<strong> > Adjusting analysis timming: </strong>');
+fprintf('<strong> > Adjusting timming: </strong>');
 c = cfg.tm;
+
+%% If start and end timepoints are zero, analize the entire epoch:
+if (c.tpstart == 0) && (c.tpend == 0)
+    c.tpstart = c.times(1);
+    c.tpend   = c.times(end);
+end
+
 %% Find closest points in the time vector:
 [~,c.startidx] = min(abs(c.times-c.tpstart));
 [~,c.endidx] = min(abs(c.times-c.tpend));

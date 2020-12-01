4c427f0377801bfe0806301eb272bd7600b3d76f
David López-García <dlopez@ugr.es>
2020-12-01 19:54:55 +0100

    GUI > plotController updated for MVCC analyses.

--- src/gui/controller/plotController.m
+++ src/gui/controller/plotController.m
@@ -2,17 +2,58 @@
 figure;
 hold on;
 
-%% 
-if cfg.classmodel.tempgen
-    if (graph.stats.above || graph.stats.below)
-        mvpalab_plottg(graph,cfg,result,stats);
+%%
+if strcmp(cfg.study.analysis,'MVPA')
+    if cfg.classmodel.tempgen
+        if (graph.stats.above || graph.stats.below)
+            mvpalab_plottg(graph,cfg,result,stats);
+        else
+            mvpalab_plottg(graph,cfg,result);
+        end
     else
-        mvpalab_plottg(graph,cfg,result);
+        if (graph.stats.above || graph.stats.below)
+            mvpalab_plotcr(graph,cfg,result,stats);
+        else
+            mvpalab_plotcr(graph,cfg,result);
+        end
     end
-else
-    if (graph.stats.above || graph.stats.below)
-        mvpalab_plotcr(graph,cfg,result,stats);
+elseif strcmp(cfg.study.analysis,'MVCC')
+    if cfg.classmodel.tempgen
+        if (graph.stats.above || graph.stats.below)
+            if isfield(result,'ab')
+                mvpalab_plottg(graph,cfg,result.ab,stats.ab);
+            end
+            if isfield(result,'ba')
+                figure;
+            hold on;
+                mvpalab_plottg(graph,cfg,result.ba,stats.ba);
+            end
+        else
+            if isfield(result,'ab')
+                mvpalab_plottg(graph,cfg,result.ab);
+            end
+            if isfield(result,'ba')
+                figure;
+            hold on;
+                mvpalab_plottg(graph,cfg,result.ba);
+            end
+        end
     else
-        mvpalab_plotcr(graph,cfg,result);
+        if (graph.stats.above || graph.stats.below)
+            if isfield(result,'ab')
+                mvpalab_plotcr(graph,cfg,result.ab,stats.ab);
+            end
+            if isfield(result,'ba')
+                mvpalab_plotcr(graph,cfg,result.ba,stats.ba);
+            end
+            
+        else
+            if isfield(result,'ab')
+                mvpalab_plotcr(graph,cfg,result.ab);
+            end
+            if isfield(result,'ba')
+                mvpalab_plotcr(graph,cfg,result.ba);
+            end
+        end
     end
 end

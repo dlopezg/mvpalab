1cd70b444a30478baadd1f04d44c41bfdf07a92b
David López-García <dlopez@ugr.es>
2021-01-11 17:16:58 +0100

    Bug fixed in mvpalab_savediag.

--- src/functions/gen/mvpalab_savediag.m
+++ src/functions/gen/mvpalab_savediag.m
@@ -14,14 +14,13 @@ end
 
 if cfg.classmodel.roc
     result = results.auc;
-    stats = statistics.auc;
     auc_dir = [cfg.study.studyLocation filesep ...
         'results' filesep 'auc' filesep 'diag'];
     mvpalab_mkdir(auc_dir);
     save([auc_dir filesep 'result.mat'],'result','cfg','-v7.3');
     
     if nargin > 2
-        stats = statistics.cr;
+        stats = statistics.auc;
         save([auc_dir filesep 'stats.mat'],'stats','-v7.3');
         save([auc_dir filesep 'permaps.mat'],'permaps','-v7.3');
     end

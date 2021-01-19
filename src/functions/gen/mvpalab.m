bc6364dc9448ed40cb313829d7060898e39e5fc2
David López-García <dlopez@ugr.es>
2021-01-19 11:58:08 +0100

    GUI > MVPAlab warning at launch if MATLAB version is older than 2017a.

--- src/functions/gen/mvpalab.m
+++ src/functions/gen/mvpalab.m
@@ -1,6 +1,19 @@
 function [] = mvpalab()
-%MVPALAB Summary of this function goes here
-%   Detailed explanation goes here
-    initView;
+if ~verLessThan('matlab','9.2') %% -v.2017a
+    initView; 
+else
+    msg = {'MVPAlab GUI has not been developed for MATLAB versions older  than 9.2 (r2017a)';
+        'You can still use MVPAlab code and functionalities but the graphic user interface may not work propertly.';
+        'Do you want to continue?'};
+    title = 'Warning';
+    opts.Default = 'Exit';
+    opts.Interpreter = 'tex';
+    res = questdlg(msg, title, 'Launch GUI', 'Exit', opts);
+    
+    if strcmp(res,'Launch GUI')
+        initView;
+    end
+end
+
 end
 

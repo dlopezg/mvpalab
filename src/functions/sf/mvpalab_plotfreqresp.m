26c2a8bfaacfc2ce964e32671300e767d8bf953c
David López-García <dlopez@ugr.es>
2021-02-02 10:03:10 +0100

    Improved mvpalab_plotfreqresp function.

--- src/functions/sf/mvpalab_plotfreqresp.m
+++ src/functions/sf/mvpalab_plotfreqresp.m
@@ -7,25 +7,96 @@ function cfg = mvpalab_plotfreqresp(cfg)
         cfg.sf.fs = 250;
     end
 
-    % Cutoff frequencies:
-    f = cfg.sf.fcutoff{1}/(cfg.sf.fs/2);
-
     % Type:
     if strcmp(cfg.sf.ftype,'bandstop')
         type = 'stop';
     else
         type = 'bandpass';
     end
+    
+    load mvpalab_colorschemes.mat
+    color = 1;
+    limit = 350;
+    figure; 
+    subplot(2,1,1)
+    hold on;
+    
+    for i = length(cfg.sf.fcutoff) : -1 : 1
+        % Generate the window:
+        cfg.sf.win = windows(cfg.sf.wtype,cfg.sf.order+1);
 
-    % Generate the window:
-    cfg.sf.win = windows(cfg.sf.wtype,cfg.sf.order+1);
+        % Calculate the filter coefficients
+        f = cfg.sf.fcutoff{i}/(cfg.sf.fs/2);
+        cfg.sf.coeff = firws(cfg.sf.order,f,type,cfg.sf.win);
 
-    % Calculate the filter coefficients
-    cfg.sf.coeff = firws(cfg.sf.order,f,type,cfg.sf.win);
+        % Plot frequency response:
+        nfft = cfg.sf.fs * 10;
+        
+        impresp = cfg.sf.coeff(:)';
+        n = length(impresp);
 
-    % Plot frequency response:
-    nfft = cfg.sf.fs*10;
-    plotfresp(cfg.sf.coeff,[],nfft,cfg.sf.fs);
+        nfft = max([2^ceil(log2(n)) nfft]);
+        f = linspace(0, cfg.sf.fs / 2, nfft / 2 + 1);
+        z = fft(impresp, nfft);
+        z = z(1:nfft / 2 + 1);
+        
+        % Color gradient:
+        shift = floor(limit/length(cfg.sf.fcutoff));
+        color = color + shift;
+        
+        % Plot magnitude response:
+        plot(f, 20 * log10(abs(z)), 'LineWidth',1,...
+            'Color', grd.neptune(limit - color,:));
+    end
+    
+    % Titles and axis:
+    title('Magnitude response');
+    ylabel('Magnitude (dB)');
+    xlabel('Frequency (Hz)');
+    xlim([0,cfg.sf.hfreq]);
+    
+    if strcmp(cfg.sf.fspac,'lin')
+        cfg.sf.fspac = 'log';
+    else
+        cfg.sf.fspac = 'lin';
+    end
+    cfg = mvpalab_genfreqvec(cfg);
+    color = 1;
+    subplot(2,1,2)
+    hold on;
+    for i = length(cfg.sf.fcutoff) : -1 : 1
+        % Generate the window:
+        cfg.sf.win = windows(cfg.sf.wtype,cfg.sf.order+1);
+
+        % Calculate the filter coefficients
+        f = cfg.sf.fcutoff{i}/(cfg.sf.fs/2);
+        cfg.sf.coeff = firws(cfg.sf.order,f,type,cfg.sf.win);
+
+        % Plot frequency response:
+        nfft = cfg.sf.fs * 10;
+        
+        impresp = cfg.sf.coeff(:)';
+        n = length(impresp);
+
+        nfft = max([2^ceil(log2(n)) nfft]);
+        f = linspace(0, cfg.sf.fs / 2, nfft / 2 + 1);
+        z = fft(impresp, nfft);
+        z = z(1:nfft / 2 + 1);
+        
+        % Color gradient:
+        shift = floor(limit/length(cfg.sf.fcutoff));
+        color = color + shift;
+        
+        % Plot magnitude response:
+        plot(f, 20 * log10(abs(z)), 'LineWidth',1,...
+            'Color', grd.neptune(limit - color,:));
+    end
+    
+    % Titles and axis:
+    title('Magnitude response');
+    ylabel('Magnitude (dB)');
+    xlabel('Frequency (Hz)');
+    xlim([0,cfg.sf.hfreq]);
 
 end
 

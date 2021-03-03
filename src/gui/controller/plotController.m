6aa8ca6d4711c6c74ecd25b565c16180b903c277
David López-García <dlopez@ugr.es>
2021-03-03 18:16:10 +0100

    plotController > Bug fixed.

--- src/gui/controller/plotController.m
+++ src/gui/controller/plotController.m
@@ -1,58 +1,74 @@
+%% Plot controller:
 
-figure;
-hold on;
-
-%%
 if strcmp(cfg.analysis,'MVPA')
     if cfg.classmodel.tempgen
         if (graph.stats.above || graph.stats.below)
+            figure; hold on;
             mvpalab_plottg(graph,cfg,result,stats);
         else
+            figure; hold on;
             mvpalab_plottg(graph,cfg,result);
         end
     else
         if (graph.stats.above || graph.stats.below)
+            figure; hold on;
             mvpalab_plotcr(graph,cfg,result,stats);
         else
+            figure; hold on;
             mvpalab_plotcr(graph,cfg,result);
         end
     end
 elseif strcmp(cfg.analysis,'MVCC')
     if cfg.classmodel.tempgen
         if (graph.stats.above || graph.stats.below)
-            if isfield(result,'ab')
+            if isstruct(result)
+                figure; hold on;
                 mvpalab_plottg(graph,cfg,result.ab,stats.ab);
-            end
-            if isfield(result,'ba')
-                figure;
-            hold on;
+                title('MVCC direction: A - B');
+                figure; hold on;
                 mvpalab_plottg(graph,cfg,result.ba,stats.ba);
+                title('MVCC direction: B - A');
+            else
+                figure; hold on;
+                mvpalab_plottg(graph,cfg,result,stats);
             end
         else
-            if isfield(result,'ab')
+            if isstruct(result)
+                figure; hold on;
                 mvpalab_plottg(graph,cfg,result.ab);
-            end
-            if isfield(result,'ba')
-                figure;
-            hold on;
+                title('MVCC direction: A - B');
+                figure; hold on;
                 mvpalab_plottg(graph,cfg,result.ba);
+                title('MVCC direction: B - A');
+            else
+                figure; hold on;
+                mvpalab_plottg(graph,cfg,result);
             end
         end
     else
         if (graph.stats.above || graph.stats.below)
-            if isfield(result,'ab')
+            if isstruct(result)
+                figure; hold on;
                 mvpalab_plotcr(graph,cfg,result.ab,stats.ab);
-            end
-            if isfield(result,'ba')
+                title('MVCC direction: A - B');
+                figure; hold on;
                 mvpalab_plotcr(graph,cfg,result.ba,stats.ba);
+                title('MVCC direction: B - A');
+            else
+                figure; hold on;
+                mvpalab_plotcr(graph,cfg,result,stats);
             end
-            
         else
-            if isfield(result,'ab')
+            if isstruct(result)
+                figure; hold on;
                 mvpalab_plotcr(graph,cfg,result.ab);
-            end
-            if isfield(result,'ba')
+                title('MVCC direction: A - B');
+                figure; hold on;
                 mvpalab_plotcr(graph,cfg,result.ba);
+                title('MVCC direction: B - A');
+            else
+                figure; hold on;
+                mvpalab_plotcr(graph,cfg,result);
             end
         end
     end

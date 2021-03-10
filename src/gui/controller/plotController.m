5c42a5bb49c36ad92d6fc481d720f0816ecab0b8
David López García <dlopez@ugr.es>
2021-03-10 10:49:45 +0100

    GUI > Impoved plotController. Now is possible to plot diferent MVCC directions independently.

--- src/gui/controller/plotController.m
+++ src/gui/controller/plotController.m
@@ -1,6 +1,26 @@
 %% Plot controller:
 
-if strcmp(cfg.analysis,'MVPA')
+%% Select MVCC direction if needed:
+
+if strcmp(cfg.analysis,'MVCC')
+    if isstruct(result)
+        if strcmp(graph.mvccDirection,'ab')
+            result = result.ab;
+            if (graph.stats.above || graph.stats.below)
+                stats = stats.ab;
+            end
+        elseif strcmp(graph.mvccDirection,'ba')
+            result = result.ba;
+            if (graph.stats.above || graph.stats.below)
+                stats = stats.ba;
+            end
+        end
+    end
+end
+    
+%% Plot MVPA and MVCC results:
+
+if strcmp(cfg.analysis,'MVPA') || strcmp(cfg.analysis,'MVCC')
     if cfg.classmodel.tempgen
         if (graph.stats.above || graph.stats.below)
             
@@ -36,120 +56,6 @@ if strcmp(cfg.analysis,'MVPA')
             
             hold on;
             mvpalab_plotcr(graph,cfg,result);
-        end
-    end
-elseif strcmp(cfg.analysis,'MVCC')
-    if cfg.classmodel.tempgen
-        if (graph.stats.above || graph.stats.below)
-            if isstruct(result)
-                
-                if ~graph.add
-                    figure;
-                end
-                
-                hold on;
-                mvpalab_plottg(graph,cfg,result.ab,stats.ab);
-                title('MVCC direction: A - B');
-                
-                if ~graph.add
-                    figure;
-                end
-                
-                hold on;
-                mvpalab_plottg(graph,cfg,result.ba,stats.ba);
-                title('MVCC direction: B - A');
-            else
-                
-                if ~graph.add
-                    figure;
-                end
-                
-                hold on;
-                mvpalab_plottg(graph,cfg,result,stats);
-            end
-        else
-            if isstruct(result)
-                
-                if ~graph.add
-                    figure;
-                end
-                
-                hold on;
-                mvpalab_plottg(graph,cfg,result.ab);
-                title('MVCC direction: A - B');
-                
-                if ~graph.add
-                    figure;
-                end
-                
-                hold on;
-                mvpalab_plottg(graph,cfg,result.ba);
-                title('MVCC direction: B - A');
-            else
-                
-                if ~graph.add
-                    figure;
-                end
-                
-                hold on;
-                mvpalab_plottg(graph,cfg,result);
-            end
-        end
-    else
-        if (graph.stats.above || graph.stats.below)
-            if isstruct(result)
-                
-                if ~graph.add
-                    figure;
-                end
-                
-                hold on;
-                mvpalab_plotcr(graph,cfg,result.ab,stats.ab);
-                title('MVCC direction: A - B');
-                
-                if ~graph.add
-                    figure; 
-                end
-                
-                hold on;
-                mvpalab_plotcr(graph,cfg,result.ba,stats.ba);
-                title('MVCC direction: B - A');
-            else
-                
-                if ~graph.add
-                    figure;
-                end
-                
-                hold on;
-                mvpalab_plotcr(graph,cfg,result,stats);
-            end
-        else
-            if isstruct(result)
-                
-                if ~graph.add
-                    figure;
-                end
-                
-                hold on;
-                mvpalab_plotcr(graph,cfg,result.ab);
-                title('MVCC direction: A - B');
-               
-                if ~graph.add
-                    figure;
-                end
-                
-                hold on;
-                mvpalab_plotcr(graph,cfg,result.ba);
-                title('MVCC direction: B - A');
-            else
-                
-                if ~graph.add
-                    figure;
-                end
-                
-                hold on;
-                mvpalab_plotcr(graph,cfg,result);
-            end
         end
     end
 end

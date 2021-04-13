5e9d122dc47e5b6646c85f125787265485d23787
David López-García <dlopez@ugr.es>
2021-04-13 13:37:45 +0200

    Added original data format and prepare Fieldtrip compatible EEG.chanlocs for the future.

--- src/functions/fe/mvpalab_dataformat.m
+++ src/functions/fe/mvpalab_dataformat.m
@@ -10,6 +10,7 @@ if isfield(data,'fsample')
     %% Fieldtrip:
     % Store sampling frequency:
     cfg.fs = data.fsample;
+    cfg.format = 'fieldtrip';
     
     % Prepare data format:
     EEG.srate = data.fsample;
@@ -17,6 +18,15 @@ if isfield(data,'fsample')
     EEG.nbchan = length(data.label);
     EEG.pnts = size(data.trial{1},2);
     EEG.times = data.time{1} * 1000;
+    
+    if isfield(data,'layout')
+        % Not ready yet:
+        % EEG.chanlocs = data.layout;
+        EEG.chanlocs = [];
+    else
+        EEG.chanlocs = [];
+    end
+    
     for trial = 1 : length(data.trial)
         EEG.data(:,:,trial) = data.trial{trial};
     end
@@ -27,6 +37,7 @@ else
     cfg.fs = data.srate;
     % Generate data file:
     EEG = data;
+    cfg.format = 'eeglab';
 end
 end
 

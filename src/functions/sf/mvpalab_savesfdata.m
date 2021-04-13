0aa5556379dd3b33a2b4fc7fdd67eac8ceda9625
David López-García <dlopez@ugr.es>
2021-04-13 12:43:22 +0200

    Sliding filter analysis > Bug fixed. Files larger than 2GB are now saved in the hard drive.

--- src/functions/sf/mvpalab_savesfdata.m
+++ src/functions/sf/mvpalab_savesfdata.m
@@ -7,14 +7,14 @@ dirname = [cfg.sf.filesLocation filesep...
     'fv' filesep 's_' sprintf('%03d',sub)];
 mvpalab_mkdir(dirname);
 filename = ['ffv_' sprintf('%03d',freq)];
-save([dirname filesep filename],'fv','cfg');
+save([dirname filesep filename],'fv','cfg','-v7.3');
 
 if cfg.sf.savefdata
     dirname = [cfg.sf.filesLocation filesep...
         'data' filesep 's_' sprintf('%03d',sub)];
     mvpalab_mkdir(dirname);
     filename = ['fdata_' sprintf('%03d',freq)];
-    save([dirname filesep filename],'data','cfg');
+    save([dirname filesep filename],'data','cfg','-v7.3');
 end
 
 end

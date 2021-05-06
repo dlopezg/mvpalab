aa60481d806f0c5794eef3ab47211e80121ee76a
David López-García <dlopez@ugr.es>
2021-05-06 11:32:45 +0200

    EEGLAB GUI is not launched when the file eeglab is called.

--- src/functions/gen/mvpalab_checkeeglab.m
+++ src/functions/gen/mvpalab_checkeeglab.m
@@ -2,8 +2,12 @@ function mvpalab_checkeeglab()
 
 if exist('eeglab.m','file')
     if ~exist('pop_firws','file')
-        warning('Starting EEGlab for filter the data');
-        eeglab;
+        warning('Initializing EEGLAB paths...');
+        eeglab nogui
+        %% Other alternative:
+        % eeglabFolder = fileparts(which('eeglab.m'));
+        % addpath(genpath(eeglabFolder));
+        % warning('EEGLAB subfolders added to the MATLAB Path.');
     end
 else
     error('EEGlab not found. EEGlab is required to compute this analysis');

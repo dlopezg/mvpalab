4854dd88ad1074c75576645e35151e751e5a9035
David López García <dlopez@ugr.es>
2021-11-24 16:47:43 +0100

    New fields included for the test windows (cfg.tm.tpstart_ and cfg.tm.tpend_). The mvpalab_timing function updates the time vector accordingly.

--- src/functions/gen/mvpalab_timing.m
+++ src/functions/gen/mvpalab_timing.m
@@ -10,14 +10,25 @@ if (c.tpstart == 0) && (c.tpend == 0)
     c.tpend   = c.times(end);
 end
 
+%% If start and end timepoints for testing window are 0, test = train:
+if (c.tpstart_ == 0) && (c.tpend_ == 0)
+    c.tpstart_ = c.tpstart;
+    c.tpend_   = c.tpend;
+end
+
 %% Find closest points in the time vector:
 [~,c.startidx] = min(abs(c.times-c.tpstart));
 [~,c.endidx] = min(abs(c.times-c.tpend));
+[~,c.startidx_] = min(abs(c.times-c.tpstart_));
+[~,c.endidx_] = min(abs(c.times-c.tpend_));
 
 %% Adjust sizes and timepoints:
 c.tpoints = (c.startidx:c.tpsteps:c.endidx);
+c.tpoints_ = (c.startidx_:c.tpsteps:c.endidx_);
 c.ntp = length(c.tpoints);
+c.ntp_ = length(c.tpoints_);
 c.times = cfg.tm.times(c.startidx:c.tpsteps:c.endidx);
+c.times_ = cfg.tm.times(c.startidx_:c.tpsteps:c.endidx_);
 
 %% Update the cfg structure
 cfg.tm = c;

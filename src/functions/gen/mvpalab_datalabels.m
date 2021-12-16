2a39c38c979d9e619b11c04c90f69fde03b3dd99
David López García <dlopez@ugr.es>
2021-12-16 12:27:44 +0100

    The mvpalab_datalabels function has been updated adding support for more than two classes.

--- src/functions/gen/mvpalab_datalabels.m
+++ src/functions/gen/mvpalab_datalabels.m
@@ -1,10 +1,25 @@
 function [X,Y,cfg] = mvpalab_datalabels(cfg,fv)
 %MVPALAB_DATALABELS Summary of this function goes here
 %   Detailed explanation goes here
-    X.a = [fv{1};fv{2}];
-    Y.a = logical([zeros(size(fv{1},1),1);ones(size(fv{2},1),1)]);
-    if length(fv) > 2
-        X.b = [fv{3};fv{4}];
-        Y.b = logical([zeros(size(fv{3},1),1);ones(size(fv{4},1),1)]);
+
+[nctxt,nclass] = size(fv);
+X.a = []; Y.a = [];
+if nctxt > 1; X.b = []; Y.b = []; end
+
+% MVPA or MVCC:
+for ctxt = 1 : nctxt
+    for class = 1 : nclass
+        if mod(class,2) == 1
+            labels = false(size(fv{ctxt,class},1),1);
+        else
+            labels = true(size(fv{ctxt,class},1),1);
+        end
+        if ctxt < 2
+            X.a = [X.a; fv{ctxt,class}];
+            Y.a = [Y.a; labels];
+        else
+            X.b = [X.b; fv{ctxt,class}];
+            Y.b = [Y.b; labels];
+        end
     end
 end

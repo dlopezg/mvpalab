5ee4cf9bf4b8a334c2f88094150fd1b4617e3a4c
David López García <dlopez@ugr.es>
2022-02-18 03:20:12 +0100

    Added mvpalab_rdm function

---
+++ src/functions/rsa/mvpalab_rdm.m
@@ -0,0 +1,42 @@
+function rdm = mvpalab_rdm(cfg,X)
+%% MVPALAB_RDM
+%
+%  This function returns the Representational Dissimilarity Matrices for
+%  in a time resolved way.
+%
+%  INPUT:
+%
+%  - X  : (MATRIX) Data matrix for an individual subject containing all the
+%         trials and conditions. [trials x chanels x timepoints]
+%
+%  OUTPUT:
+%
+%  - rdm : (3D-MATRIX) Representational Dissimilarity Matrices for each
+%          timepoint [trials x trials x timepoints]
+
+%% Measure the elapsed time:
+
+tic;
+
+%% Construct RDMs using Pearson correlation:
+if strcmp(cfg.rsa.method,'pearson')
+    parfor tp = 1 : cfg.tm.ntp
+        X_tp = X(:,:,tp);
+        rdm(:,:,tp) = 1 - corrcoef(X_tp');
+    end
+end
+
+%% Construct RDMs using the euclidean distance:
+if strcmp(cfg.rsa.method,'euclidean')
+    parfor tp = 1 : cfg.tm.ntp
+        X_tp = X(:,:,tp);
+        rdm(:,:,tp) = pdist2(X_tp,X_tp,'euclidean');
+    end
+end
+
+%% Done!
+
+toc
+
+end
+

778fa084ff1d31230a1fc20c3dda4ff5ee280608
David López García <dlopez@ugr.es>
2022-02-18 01:27:03 +0100

    mvpalab_findbound function included. This function returns the indexes of the last and middle trials of each condition.

---
+++ src/functions/rsa/mvpalab_findbound.m
@@ -0,0 +1,37 @@
+function bounds = mvpalab_findbound(Y)
+%% MVPALAB_FINDBOUNDS
+%
+%  This function returns the indexes of the last trials of each condition.
+%  It additionally returns the index of the middle trial of each condition.
+%
+%  INPUT:
+%
+%  - Y  : (STRUCT) Label vector for an individual subject.
+%
+%  OUTPUT:
+%
+%  - bounds : (STRUCT) 
+
+
+%% Find boundaries:
+%  Find changes in the value of consecutive elements.
+%  (from 0 to 1 or viceversa).
+
+last = find(diff(Y)~=0);
+last(end+1) = length(Y);
+
+%% Find middle trials:
+
+last_ = [0; last];
+
+for i = 2 : length(last_)
+    middle(i-1) = ceil((last_(i) - last_(i-1)) / 2) + last_(i-1);
+end
+
+%% Return boundaries:
+
+bounds.last = last;
+bounds.middle = middle;
+
+end
+

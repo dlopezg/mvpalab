d3dde5818c487af25ca2925c3b75a3d86c90cbdc
David López García <dlopez@ugr.es>
2022-02-19 02:26:15 +0100

    Added support for permutations in RSA analysis

--- src/functions/rsa/mvpalab_computersa.m
+++ src/functions/rsa/mvpalab_computersa.m
@@ -1,4 +1,4 @@
-function corrseries = mvpalab_computersa(mtx,mtx_)
+function corrseries = mvpalab_computersa(cfg,mtx,mtx_,permute)
 %% MVPALAB_COMPUTERSA
 %
 % This function computes the second order representational similarity
@@ -8,60 +8,86 @@ function corrseries = mvpalab_computersa(mtx,mtx_)
 
 %  INPUT:
 %
-%  - mtx  : (2D - MATRIX) Data matrix including the empirical vectoriced 
+%  - mtx  : (2D - MATRIX) Data matrix including the empirical vectoriced
 %           RDMs for each timepoint: [timepoint x vectorized]
 %
-%  - mtx_ : (3D - MATRIX) Data matrix including the vectorized RDMs for 
-%           each theoretical model and timepoint: 
+%  - mtx_ : (3D - MATRIX) Data matrix including the vectorized RDMs for
+%           each theoretical model and timepoint:
 %           [timepoint x vectorized x model]
 %
 %  OUTPUT:
 %
-%  - corrseries : (4D-MATRIX) - Time series including correlation values
+%  - corrseries : (5D-MATRIX) - Time series including correlation values
 %           for each timepoint and model. For data structure consistency:
-%           [1 x timepoints x 1 x model]
+%           [1 x timepoints x 1 x permutetions x model]
+
+if permute
+    fprintf('\n     <strong>- Computing permuted maps:</strong>\n');
+else
+    fprintf('\n     <strong>- Computing second order RSA:</strong>\n');
+end
 
 
+%% Permute or not.
+%  Calculate the number of permutations
+
+if permute, nperm = cfg.stats.nper; else, nperm = 1; end
+
+
+%% Model loop:
+%  Description:
 for mdl = 1 : size(mtx_,3)
     
-    %% Compute correlation:
-    %  The Spearman correlation is computed for each timepoint and the
-    %  time series is stored in r.
+    fprintf(['        # Model: ' cfg.rsa.tmodels{mdl}.id '... ']);
+    if permute,fprintf('- Permutation: '); end
     
-    r = [];
-    
-    parfor tp = 1 : size(mtx,1)
+    %% Permutation loop:
+    %  Description:
+    for perm = 1 : nperm
+        %% Compute correlation:
+        %  The Spearman correlation is computed for each timepoint and the
+        %  time series is stored in r.
         
-        trdm = mtx_(tp,:,mdl);
-        rdm = mtx(tp,:);
-        r(tp) = corr(rdm',trdm','Type','Spearman');
+        r = [];
         
+        parfor tp = 1 : size(mtx,1)
+            
+            trdm = mtx_(tp,:,mdl);
+            rdm = mtx(tp,:);
+            if permute, rdm = rdm(randperm(length(rdm))); end
+            r(tp) = corr(rdm',trdm','Type','Spearman');
+            
+        end
+        
+        %% Search and correct extreme correlations:
+        %  Force finite values for later z-transformation: +1/-1 correlation
+        %  values should be corrected to +/-.999999 to avoid infinity for
+        %  z-transformed correlations.
+        %  EPS corrects for rounding errors in r. EPS returns the distance from
+        %  1.0 to the next larger double-precision number.
+        
+        r_ = (abs(r) + eps) >= 1;
+        
+        if any(r_(:))
+            
+            warning('Correlations of +1 or -1 found. Correcting to +/-0.9999');
+            r(r_) = 0.99999*r(r_);
+            
+        end
+        
+        %% Fisher transformation:
+        %  Translate to Fisher's z transformed values. When this
+        %  transformation is applied the sampling distribution of the
+        %  resulting variable is approximately normal, with a variance
+        %  that is stable over different values of the underlying true
+        % correlation.
+        
+        corrseries(1,:,1,perm,mdl) = atanh(r);
+        
+        if permute, mvpalab_pcounter(perm,nperm); end
     end
     
-    %% Search and correct extreme correlations:
-    %  Force finite values for later z-transformation: +1/-1 correlation
-    %  values should be corrected to +/-.999999 to avoid infinity for 
-    %  z-transformed correlations.
-    %  EPS corrects for rounding errors in r. EPS returns the distance from 
-    %  1.0 to the next larger double-precision number.
-    
-    r_ = (abs(r) + eps) >= 1; 
-    
-    if any(r_(:))
-        
-        warning('Correlations of +1 or -1 found. Correcting to +/-0.9999');
-        r(r_) = 0.99999*r(r_);
-        
-    end
-    
-    %% Fisher transformation:
-    %  Translate to Fisher's z transformed values. When this
-    %  transformation is applied the sampling distribution of the
-    %  resulting variable is approximately normal, with a variance
-    %  that is stable over different values of the underlying true
-    % correlation.
-    
-    corrseries(1,:,1,mdl) = atanh(r);
+    fprintf('- Done.\n');
     
 end
 
--- src/functions/rsa/mvpalab_vectorizerdm.m
+++ src/functions/rsa/mvpalab_vectorizerdm.m
@@ -1,28 +1,35 @@
 function vrdm = mvpalab_vectorizerdm(cfg,rdm)
 %% MVPALAB_VECTORIZERDM
 %
-%  This function returns the vectorized version of a temporal series of 
+%  This function returns the vectorized version of a temporal series of
 %  RDMs in the form: [timepoints x vectorizedrdm]
 
 %%  INPUT:
 %
-%  - cfg: (STRUCT) Configuration estructure. 
+%  - {struct} cfg
+%    Description: Configuration structure.
 %
-%  - rdm : (4D-MATRIX) Representational Dissimilarity Matrices for each
-%          timepoint and model [trials x trials x timepoints x model]
+%  - {4D-matrix} rdm
+%    Description: Representational Dissimilarity Matrices for each
+%    timepoint and model. - [trials x trials x timepoints x model]
 %
 %%  OUTPUT:
 %
-%  - vrdm : (3D-MATRIX) Vecotized version of a temporal series of RDMs
-%           in the form: [timepoints x vectorizedrdm x models]
+%  - {3D-matrix} vrdm
+%    Description: Vectorized version of a temporal series of RDMs in the
+%    form: [timepoints x vectorizedrdm x models]
+
+fprintf('\n     <strong>- Vectorizing empirical and theoretical RDMs...</strong>\n');
 
 %% Remove diagonal and upper triangle:
 %  RDMs are square and symmetrical matrices along its diagonal. For
 %  computational efficiency both the diagonal and the upper triangle can be
-%  removed for each timepoint. 
-% The result matrix is [timepoints x values x models]
+%  removed for each timepoint.
 
 for mdl = 1 : size(rdm,4)
+    if size(rdm,4) > 1
+        fprintf(['        # Model: ' cfg.rsa.tmodels{mdl}.id '... ']);
+    end
     parfor tp = 1 : size(rdm,3)
         
         rdm_ = rdm(:,:,tp,mdl);
@@ -30,6 +37,11 @@ for mdl = 1 : size(rdm,4)
         rdm_(logical(eye(size(rdm_)))) = 0;
         vrdm(tp,:,mdl) = squareform(rdm_);
     end
+    
+    if size(rdm,4) > 1
+        fprintf('- Done.\n');
+    end
+    
 end
 
 %% Repmat models:
@@ -37,7 +49,7 @@ end
 %  for each timepoint:
 
 if size(rdm,3) == 1
-   vrdm = repmat(vrdm(1,:,:),[cfg.tm.ntp 1 1]); 
+    vrdm = repmat(vrdm(1,:,:),[cfg.tm.ntp 1 1]);
 end
 
 end

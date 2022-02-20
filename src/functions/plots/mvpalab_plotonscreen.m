13f2125385d3ab10339abaa69971752d266c65db
David López García <dlopez@ugr.es>
2022-02-20 02:04:02 +0100

    mvpalab_onscreen function updated and adapted for RSA analysis

--- src/functions/plots/mvpalab_plotonscreen.m
+++ src/functions/plots/mvpalab_plotonscreen.m
@@ -1,7 +1,7 @@
 function [] = mvpalab_plotonscreen(graphs)
 %mvpalab_plotonscreen Summary of this function goes here
 %   Detailed explanation goes here
-rectangle('Position',[graphs.onscreen(1) 0 graphs.onscreen(2) 1],...
+rectangle('Position',[graphs.onscreen(1) -1.5 graphs.onscreen(2) 3],...
     'facecolor',[.9 .9 .9],'edgecolor','w');
 hl = vline(0,'k-','On screen time');
 

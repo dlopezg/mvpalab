a1dbb0b807fae27afbdab010fc8c1bbf604217f3
David López García <dlopez@ugr.es>
2022-04-01 17:45:25 +0200

    Function mvpalab_readnifti added. This function reads the specified NIFTI file using MATLAB or SPM.

---
+++ src/functions/searchlight/mvpalab_readnifti.m
@@ -0,0 +1,50 @@
+function volume = mvpalab_readnifti(file)
+%% MVPALAB_READNIFTI
+%
+%  This function load the specified NIFTI file using the matlab function
+%  niftiread if it is available (matlab version R2017b and avobe).
+%
+%  Otherwise the SPM function spm_vol is employed. Note that SPM must be
+%  installed and added to the matlab path.
+%
+%%  INPUT:
+%
+%  - {struct} - file:
+%    Configuration structure.
+%
+%%  OUTPUT:
+%
+%  - {struct} - data:
+%    Mask file containing the following fields:
+%       mask.data:  [3D-matrix] containing the mask voxels.
+%       mask.dim:   [vector]    containing the size of the mask.
+%       mask.fname: [string]    containing the path to the mask file.
+%%
+
+if exist('niftiread','file')
+    
+    % Read nifti header and get info:
+    header = niftiinfo(file);
+    volume.dim = header.ImageSize;
+    volume.fname = header.Filename;
+    
+    % Read volume data:
+    volume.data = niftiread(file);
+    
+    
+elseif exist('spm_vol','file')
+    
+    % Read nifti header and get info:
+    header = spm_vol(file);
+    volume.dim = header.dim;
+    volume.fname = header.fname;
+    
+    % Read volume data:
+    volume.data = spm_read_vols(header);
+    
+else
+    error('Error: No software found to read .nii files.')
+end
+
+end
+

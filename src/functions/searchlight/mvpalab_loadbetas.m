c8fc05fbb8576967a90be93583421cb04e6cba8c
David López García <dlopez@ugr.es>
2022-04-17 20:06:23 +0200

    regressor_names.mat no longer needeed

--- src/functions/searchlight/mvpalab_loadbetas.m
+++ src/functions/searchlight/mvpalab_loadbetas.m
@@ -8,7 +8,6 @@ function betas = mvpalab_loadbetas(cfg)
 %% Load SMP file and regressors names:
 
 load(fullfile(cfg.study.SPMFolder,'SPM.mat'));
-load(fullfile(cfg.study.SPMFolder,'regressor_names.mat'));
 
 %% Extract beta files and info:
 
@@ -22,19 +21,13 @@ for i = 1 : length(cfg.study.conditionIdentifier)
     
     % Extract regressor name:
     regressor_name = cfg.study.conditionIdentifier{1,i};
-    
-    % Look for regressor_name in the regressor_names cell array:
-    if exist('regressor_names','var')
-        idxs = strcmp(regressor_names(1,:), regressor_name);
-    else
-        error('regressor_names file not found.');
-    end
+    idxs = contains(betas_descrip,regressor_name);
     
     % Generate files to load:
     files_to_load = fullfile(cfg.study.SPMFolder,betas_fname(idxs))';
     
     for f = 1 : length(files_to_load)
-       betas{i,f} = mvpalab_readnifti(files_to_load{f}); 
+        betas{i,f} = mvpalab_readnifti(files_to_load{f});
     end
     
 end
